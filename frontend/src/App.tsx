aa857800a97210512cb6ecdffef1fd81ee69a177
ac2393921 <ac2393921@gmail.com>
2021-11-03 23:34:47 +0900

    add: Todoの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Header } from './compornets/Header';
+import { TodoList } from './compornets/TodoList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TodoList todos={[
+        {title: "Do dishes", description: "Do dishes", isCompleted: true},
+        {title: "Do dishes", description: "Do dishes", isCompleted: true},
+      ]} />
     </div>
   );
 }
